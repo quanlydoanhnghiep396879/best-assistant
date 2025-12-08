1bc3e9c60cf06df971d22cbc638f9a91ec517a9b
quanlydoanhnghiep396879 <quanlydoanhnghiep396879@gmail.com>
2025-12-09 03:19:09 +0700

    fix runtime google api

--- app/api/check-kpi/route.js
+++ app/api/check-kpi/route.js
@@ -1,6 +1,9 @@
 import { NextResponse } from "next/server";
 import { google } from "googleapis";
 
+export const dynamic = "force-dynamic";
+export const runtime = "edge";
+
 export async function POST() {
   try {
     // AUTH GOOGLE SHEETS
