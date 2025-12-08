a813850a6ec3851cf480e63afc42a8b7ddb9ccfc
quanlydoanhnghiep396879 <quanlydoanhnghiep396879@gmail.com>
2025-12-09 02:30:54 +0700

    real space

--- app/api/check-kpi/route.js
+++ app/api/check-kpi/route.js
@@ -7,7 +7,7 @@ export async function POST() {
     const auth = new google.auth.JWT(
       process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
       null,
-      process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      process.env.GOOGLE_PRIVATE_KEY,
       ["https://www.googleapis.com/auth/spreadsheets.readonly"]
     );
 
