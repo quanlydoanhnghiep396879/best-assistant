8e92ba635eac5d86497615c86a6d33e6784ba8f6
quanlydoanhnghiep396879 <quanlydoanhnghiep396879@gmail.com>
2025-12-08 21:00:10 +0700

    checklist and fix code

--- app/api/check-kpi/route.js
+++ app/api/check-kpi/route.js
@@ -5,7 +5,7 @@ export async function POST() {
   try {
     // AUTH GOOGLE SHEETS
     const auth = new google.auth.JWT(
-      process.env.GOOGLE_CLIENT_EMAIL,
+      process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
       null,
       process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
       ["https://www.googleapis.com/auth/spreadsheets.readonly"]
