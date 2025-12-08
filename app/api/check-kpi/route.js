5e754ff1c6f697c691a067b5b5526160c82aff71
quanlydoanhnghiep396879 <quanlydoanhnghiep396879@gmail.com>
2025-12-09 04:59:38 +0700

    fix vercel

--- app/api/check-kpi/route.js
+++ app/api/check-kpi/route.js
@@ -12,7 +12,7 @@ export async function POST() {
     const auth = new google.auth.JWT(
       process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
       null,
-      process.env.GOOGLE_PRIVATE_KEY,
+      process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
       ["https://www.googleapis.com/auth/spreadsheets.readonly"]
     );
 
