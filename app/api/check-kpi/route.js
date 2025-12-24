48c79ee1acf4125772eca0fa954649316b180c1d
quanlydoanhnghiep396879 <quanlydoanhnghiep396879@gmail.com>
2025-12-24 20:36:36 +0700

    new5

--- app/api/check-kpi/route.js
+++ app/api/check-kpi/route.js
@@ -1,6 +1,9 @@
 import { NextResponse } from "next/server";
 import { google } from "googleapis";
 import nodemailer from "nodemailer";
+function writeLog(...args){
+  console.log("[KPI]", ...args);
+}
 
 export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
