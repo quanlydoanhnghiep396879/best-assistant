dd5a5d80d36cd76022b2e0a0271bb7ed9d9b68a7
quanlydoanhnghiep396879 <quanlydoanhnghiep396879@gmail.com>
2025-12-30 10:18:32 +0700

    haruharu

--- app/kpi/page.js
+++ app/kpi/page.js
@@ -1,7 +1,7 @@
 export const dynamic = "force-dynamic";
-export const revalidate = 0;
 
-import KpiDashboardClient from "../KpiDashboardClient";
+
+import KpiDashboardClient from "./KpiDashboardClient";
 
 export default function Page() {
   return <KpiDashboardClient />;
--- app/page.js
+++ app/page.js
@@ -1,8 +1,3 @@
-import KpiDashboardClient from "./KpiDashboardClient";
-
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
-export default function Page() {
-  return <KpiDashboardClient />;
+export default function Home() {
+  return <main>Home</main>;
 }
