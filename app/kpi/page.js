a4554a244181b484b207b7dd0458b2b405b89a23
quanlydoanhnghiep396879 <quanlydoanhnghiep396879@gmail.com>
2026-01-01 22:20:16 +0700

    lsls

--- app/kpi/page.js
+++ app/kpi/page.js
@@ -1,4 +1,6 @@
-// app/kpi/page.js
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import KpiDashboardClient from "./KpiDashboardClient";
 
 export default function Page() {
